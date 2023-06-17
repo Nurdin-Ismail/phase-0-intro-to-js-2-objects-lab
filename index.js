0aed6d3293d296e500814bf91eb0172b4bca66a8
Nurdin <nurdin.ismail@student.moringaschool.com>
2023-06-17 20:34:10 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+const employee = {
+    name: "Nur",
+    street: "Nyeri rd"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newobj ={...employee};
+    newobj[key] = value;
+
+    return newobj;
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return employee
+
+
+}
+
+function deleteFromEmployeeByKey(employee, k){
+    const kanu = {...employee}
+    delete kanu[k]
+    return kanu
+
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+    
+
+
+
+
+}
